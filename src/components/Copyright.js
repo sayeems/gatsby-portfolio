41c4b2901dec3b280be42e581ca6df15ec7c8cfa
sayeem <sayeem.shahria@pantheon.io>
2022-08-17 20:53:09 +0600

    trigger build

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -3,7 +3,7 @@ import React from "react"
 export const Copyright = () => {
   return (
     <div className="copyrights">
-      &copy; {new Date().getFullYear()} All rights reserved
+      &copy; {new Date().getFullYear()} All rights reserved -
     </div>
   )
 }
